3f4cb59007edf83033551738adb30315af3523e0
channz <82771025+channz@users.noreply.github.com>
2021-12-23 09:51:49 +0700

    Add files via upload

---
+++ bloodbank/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer text-center bg-light">
+	© Copyright <span id="demo"></span> <span class="brand">BankDarah</span>
+	<script>
+		date = new Date();
+    	document.getElementById("demo").innerHTML=date.getFullYear();
+    </script>
+</footer>
---
+++ bloodbank/index.php
@@ -0,0 +1,145 @@
+<?php 
+    session_start();
+
+    ?>
+<!DOCTYPE html>
+<html>
+<?php $title="Bank Darah | home page"; ?>
+<?php require 'head.php'; ?>
+<body>
+    <?php require 'header.php'; ?>
+
+    <div class="container cont">
+
+   <?php require 'message.php'; ?>
+
+        <div class="row justify-content-center">
+            <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5 col-xs-6 mb-5" style="width: 60%">
+                <div class="card">
+                    <img src="image/bg.png" class="card-img-top">
+                </div>
+            </div>
+
+            <div class="col-lg-9">
+            <div class="row">
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">A+</div>
+                    <div class="card-body">
+                        Jika Anda A +: Anda dapat memberikan darah ke A + dan AB +. Anda dapat menerima darah dari A +, A-, O + dan O-.
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">A-</div>
+                    <div class="card-body">
+                        Jika Anda A-: Anda dapat memberikan darah ke A-, A +, AB- dan AB +. Anda dapat menerima darah dari A- dan O-. 
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">B+</div>
+                    <div class="card-body">
+                         Anda bisa memberikan darah ke A + dan AB +. Anda dapat menerima darah dari A +, A-, O + dan O-.
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">B-</div>
+                    <div class="card-body">
+                        Jika Anda B-: Anda dapat memberikan darah ke B-, B +, AB + dan AB-, Anda dapat menerima darah dari B- dan O-. Anda dapat memberikan darah ke B + dan AB +.
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">AB+</div>
+                    <div class="card-body">
+                         Orang dengan darah AB + dapat menerima sel darah merah dari semua golongan darah. Artinya permintaan AB + bisa berdonasi dengan AB saja.
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">AB-</div>
+                    <div class="card-body"> 
+                         AB- pasien dapat menerima sel darah merah dari semua golongan darah negatif.
+                         AB- dapat memberikan sel darah merah ke golongan darah AB- dan AB +.
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">O+</div>
+                    <div class="card-body">
+                        Darah O + dapat disumbangkan ke A +, B +, AB + dan O + Darah
+                        Grup O dapat mendonasikan sel darah merah kepada siapa saja. Itu adalah donor universal.
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">O-</div>
+                    <div class="card-body">
+                        O- dapat berdonasi ke A +, A-, B +, B-, AB +, AB-, O + dan O- Darah
+                        Orang dengan darah O negatif hanya dapat menerima sumbangan sel darah merah dari donor O negatif.
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 mb-5">
+                <div class="card">
+                    <div class="card-header text-center">Kontak Admin</div>
+                    <div class="card-body">
+                        <i class="fa fa-envelope"> </i> <a > pmi@organization.com</a><br><br>
+                        <i class="fa fa-mobile"> </i> <a > +628123456789</a><br><br>
+                    </div>
+                </div>
+            </div>
+            </div>
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-lg-6 rounded mb-5">
+
+            </div>
+            <div class="col-lg-6 rounded mb-5">
+                 </div>
+        </div>
+
+        <div class="row mb-5">
+            <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">Health tips</div>
+                <div class="card-body">
+                    <dt>Makan makanan yang sehat</dt>
+                    <dd>Makan kombinasi makanan yang berbeda, termasuk buah, sayuran, kacang-kacangan, kacang-kacangan, dan biji-bijian. Orang dewasa harus makan setidaknya lima porsi (400g) buah dan sayuran per hari. Anda dapat meningkatkan asupan buah dan sayuran dengan selalu memasukkan sayuran ke dalam makanan Anda; makan buah dan sayuran segar sebagai makanan ringan; makan berbagai buah dan sayuran; dan memakannya pada musimnya. Dengan makan sehat, Anda akan mengurangi risiko kekurangan gizi dan penyakit tidak menular (PTM) seperti diabetes, penyakit jantung, stroke, dan kanker.</dd>
+                    <dt> Kurangi konsumsi garam dan gula</dt>
+                    <dd>Di sisi lain, mengonsumsi gula dalam jumlah berlebihan meningkatkan risiko kerusakan gigi dan penambahan berat badan yang tidak sehat. Pada orang dewasa dan anak-anak, asupan gula bebas harus dikurangi hingga kurang dari 10% dari total asupan energi. Ini setara dengan 50g atau sekitar 12 sendok teh untuk orang dewasa. WHO merekomendasikan mengkonsumsi kurang dari 5% dari total asupan energi untuk manfaat kesehatan tambahan. Anda dapat mengurangi asupan gula dengan membatasi konsumsi camilan manis, permen, dan minuman manis.</dd>
+                    <dt>Aktif</dt>
+                    <dd>Aktivitas fisik didefinisikan sebagai setiap gerakan tubuh yang dihasilkan oleh otot rangka yang membutuhkan pengeluaran energi. Ini termasuk olahraga dan aktivitas yang dilakukan saat bekerja, bermain, melakukan pekerjaan rumah tangga, bepergian, dan terlibat dalam kegiatan rekreasi. Jumlah aktivitas fisik yang Anda butuhkan tergantung pada kelompok usia Anda, tetapi orang dewasa berusia 18-64 tahun harus melakukan setidaknya 150 menit aktivitas fisik intensitas sedang sepanjang minggu. Tingkatkan aktivitas fisik intensitas sedang menjadi 300 menit per minggu untuk manfaat kesehatan tambahan.</dd>
+                    <dt>Jangan merokok</dt>
+                    <dd>Merokok tembakau menyebabkan PTM seperti penyakit paru-paru, penyakit jantung dan stroke. Tembakau membunuh tidak hanya perokok langsung tetapi bahkan non-perokok melalui paparan tangan kedua. Saat ini, ada sekitar 15,9 juta orang dewasa Filipina yang merokok tembakau tetapi 7 dari 10 perokok tertarik atau berencana untuk berhenti.
+                    Jika saat ini Anda seorang perokok, belum terlambat untuk berhenti. Setelah Anda melakukannya, Anda akan merasakan manfaat kesehatan langsung dan jangka panjang. Jika Anda bukan perokok, itu bagus! Jangan mulai merokok dan perjuangkan hak Anda untuk menghirup udara bebas asap rokok.</dd>
+                    <dt>Hanya minum air yang aman</dt>
+                    <dt>Dapatkan ujian</dt>
+                    <dt>Ikuti peraturan lalu lintas</dt>
+                    <dt>Bicaralah dengan seseorang yang Anda percayai jika Anda merasa sedih</dt>
+                    <dt>Bersihkan tangan dengan benar</dt>
+                    <dt>Lakukan pemeriksaan rutin</dt>
+                    Kunjungi di sini untuk mendapatkan tips kesehatan lainnya.
+                    <a href="https://www.who.int/philippines/news/feature-stories/detail/20-health-tips-for-2020" target="_blank"> World Health Organisation</a>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+
+    <?php require 'footer.php'; ?>
+
+</body>
+</html>
